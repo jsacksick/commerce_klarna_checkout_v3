29641ac5f5b867b54d617ef326bbf1094a38d072
Jonathan Sacksick <jonathan.sacksick@gmail.com>
2019-12-12 22:52:19 +0200

    Make acknowledge order protected.

--- src/Plugin/Commerce/PaymentGateway/KlarnaCheckout.php
+++ src/Plugin/Commerce/PaymentGateway/KlarnaCheckout.php
@@ -276,9 +276,24 @@ class KlarnaCheckout extends OffsitePaymentGatewayBase implements KlarnaCheckout
   }
 
   /**
-   * {@inheritdoc}
+   * Acknowledge the given Klarna order, and optionally capture the payment if
+   * configured to do so.
+   *
+   * @param string $klarna_order_id
+   *   The Klarna order ID.
+   * @param \Drupal\commerce_order\Entity\OrderInterface $order
+   *   (optional) Optionally pass the order when available. If not passed, the
+   *   order ID will be obtain from the "merchant_reference2" property passed
+   *   to Klarna when creating the order.
+   *
+   * @return \Drupal\commerce_payment\Entity\PaymentInterface
+   *   The "authorized"|"captured" payment transaction created.
+   *
+   * @throws \Drupal\commerce_payment\Exception\PaymentGatewayException
+   *   In case the Klarna order could not be loaded or if its status is not the
+   *   expected one.
    */
-  public function acknowledgeOrder($klarna_order_id, OrderInterface $order = NULL) {
+  protected function acknowledgeOrder($klarna_order_id, OrderInterface $order = NULL) {
     $klarna_manager = $this->klarnaManagerFactory->get($this->getConfiguration());
 
     try {
--- src/Plugin/Commerce/PaymentGateway/KlarnaCheckoutInterface.php
+++ src/Plugin/Commerce/PaymentGateway/KlarnaCheckoutInterface.php
@@ -2,33 +2,10 @@
 
 namespace Drupal\commerce_klarna_checkout\Plugin\Commerce\PaymentGateway;
 
-use Drupal\commerce_order\Entity\OrderInterface;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayInterface;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\SupportsAuthorizationsInterface;
 
 /**
  * Provides the interface for the Klarna Checkout payment gateway.
  */
-interface KlarnaCheckoutInterface extends OffsitePaymentGatewayInterface, SupportsAuthorizationsInterface {
-
-  /**
-   * Acknowledge the given Klarna order, and optionally capture the payment if
-   * configured to do so.
-   *
-   * @param string $klarna_order_id
-   *   The Klarna order ID.
-   * @param \Drupal\commerce_order\Entity\OrderInterface $order
-   *   (optional) Optionally pass the order when available. If not passed, the
-   *   order ID will be obtain from the "merchant_reference2" property passed
-   *   to Klarna when creating the order.
-   *
-   * @return \Drupal\commerce_payment\Entity\PaymentInterface
-   *   The "authorized"|"captured" payment transaction created.
-   *
-   * @throws \Drupal\commerce_payment\Exception\PaymentGatewayException
-   *   In case the Klarna order could not be loaded or if its status is not the
-   *   expected one.
-   */
-  public function acknowledgeOrder($klarna_order_id, OrderInterface $order = NULL);
-
-}
+interface KlarnaCheckoutInterface extends OffsitePaymentGatewayInterface, SupportsAuthorizationsInterface {}
